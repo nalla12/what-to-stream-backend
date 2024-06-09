2d2d512fe176122dae83b2fd2219c0adbd5c8483
Allan Duong <alland12@hotmail.com>
2024-06-09 03:08:54 +0200

    Add API ShowController

---
+++ WhatToStreamBackend/Controllers/ShowsController.cs
@@ -0,0 +1,121 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WhatToStreamBackend.Models;
+
+namespace WhatToStreamBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShowsController : ControllerBase
+    {
+        private readonly ShowsDbContext _context;
+
+        public ShowsController(ShowsDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Shows
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Show>>> GetShows()
+        {
+            return await _context.Shows.ToListAsync();
+        }
+
+        // GET: api/Shows/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Show>> GetShow(string id)
+        {
+            var show = await _context.Shows.FindAsync(id);
+
+            if (show == null)
+            {
+                return NotFound();
+            }
+
+            return show;
+        }
+
+        // PUT: api/Shows/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutShow(string id, Show show)
+        {
+            if (id != show.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(show).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ShowExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Shows
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Show>> PostShow(Show show)
+        {
+            _context.Shows.Add(show);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (ShowExists(show.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetShow", new { id = show.Id }, show);
+        }
+
+        // DELETE: api/Shows/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteShow(string id)
+        {
+            var show = await _context.Shows.FindAsync(id);
+            if (show == null)
+            {
+                return NotFound();
+            }
+
+            _context.Shows.Remove(show);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ShowExists(string id)
+        {
+            return _context.Shows.Any(e => e.Id == id);
+        }
+    }
+}
